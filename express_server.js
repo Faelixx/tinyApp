cab495ca194e86b66cd6d5e55804b782122c198e
Faelixx <sasha.taylor7@gmail.com>
2023-11-25 23:54:03 -0700

    Feat: Add new url generation page

--- express_server.js
+++ express_server.js
@@ -18,10 +18,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase }
   res.render("urls_show", templateVars);
 });
+
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
